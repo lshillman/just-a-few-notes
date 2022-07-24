fa20136d6fde3cebbe48b0f411abf9322afc2076
lshillman <lshillman@users.noreply.github.com>
2022-07-24 11:49:48 -0700

    add helper functions for read / readAndAppend / write

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,20 @@
 const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
     console.log("Page is attempting to read notes");
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+
+
+
+
+
+
+
+
 
 // POST Route for a new note
 notes.post('/', (req, res) => {
