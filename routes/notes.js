7e481ad69400ba4378370840b81287ccfa9f875d
lshillman <lshillman@users.noreply.github.com>
2022-07-24 22:20:47 -0700

    add middleware for decoding requests. Add uuid package for generating unique note IDs.

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,12 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
     console.log("Page is attempting to read notes");
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 
@@ -18,23 +20,23 @@ notes.get('/', (req, res) => {
 
 // POST Route for a new note
 notes.post('/', (req, res) => {
-  console.log("Save button clicked! " + req.body);
+  console.log("Save button clicked! " + req.body.title);
+  res.send(req.body);
+  const { title, text } = req.body;
+  console.log(title + ", " + text)
 
-//   const { username, topic, tip } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
 
-//   if (req.body) {
-//     const newTip = {
-//       username,
-//       tip,
-//       topic,
-//       tip_id: uuidv4(),
-//     };
-
-//     readAndAppend(newTip, './db/db.json');
-//     res.json(`Note added successfully 🚀`);
-//   } else {
-//     res.error('Error adding note');
-//   }
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error adding note');
+  }
 });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const api = require('./routes/index.js');
 const PORT = process.env.port || 3001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/api', api);
 
